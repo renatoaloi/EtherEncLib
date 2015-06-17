bf0f1c49c2d25c68b6f0566570bf3257b11a9a71
renato.aloi <renato.aloi@gmail.com>
2015-06-16 21:33:23 -0300

    applying license to files

--- checksum.c
+++ checksum.c
@@ -1,3 +1,23 @@
+/*
+    EtherEncLib - Ethernet ENC28J60 Library for Arduino
+    Copyright (C) 2015  Renato Aloi
+    renato.aloi@gmail.com -- http://www.seriallink.com.br
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include "checksum.h"
 
 // The Ip checksum is calculated over the ip header only starting
